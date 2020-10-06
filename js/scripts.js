a53cedeacb7d709241b810602f0cf9cda937e4d8
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2020-10-05 23:06:39 -0400

    added first three pokemon to pokemonList

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+let pokemonList = [
+	{
+		name: "Pidgey",
+		height: "1' 00\"",
+		types: ["normal", "flying"],
+	},
+	{
+		name: "Pidgeotto",
+		height: "3' 07\"",
+		types: ["normal", "flying"],
+	},
+	{
+		name: "Pidgeot",
+		height: "4' 11\"",
+		types: ["normal", "flying"],
+	},
+];
