ba4b91c38a460e2396e8d2a3550424ad2c49addd
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2020-10-08 16:26:29 -0400

    Refactored previous code

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,83 @@
-let pokemonList = [
-	{
-		name: "Pidgey",
-		height: "1' 00\"",
-		types: ["normal", "flying"],
-	},
-	{
-		name: "Pidgeotto",
-		height: "3' 07\"",
-		types: ["normal", "flying"],
-	},
-	{
-		name: "Pidgeot",
-		height: "4' 11\"",
-		types: ["normal", "flying"],
-	},
-];
-
-for (let i = 0; i < pokemonList.length; i++) {
-	let convertToMeters = function (height) {
-		let feet = Number(pokemonList[i].height.match(/(\d+)'/)[1]);
-		let inches = Number(pokemonList[i].height.match(/(\d\d)"$/)[1]);
-		return ((feet * 12 + inches) / 39.37).toFixed(2);
-	};
-	if (convertToMeters(pokemonList[i].height) < 1.25) {
-		document.write(
-			`<h1>${pokemonList[i].name} has a height of ${convertToMeters(
-				pokemonList[i].height
-			)} meters</h1>`
-		);
-	} else {
-		document.write(
-			`<h1>${pokemonList[i].name} has a height of ${convertToMeters(
-				pokemonList[i].height
-			)} meters - Wow, that's big!</h1>`
-		);
+let pokemonRepository = (function () {
+	let pokemonList = [
+		{
+			name: "Pidgey",
+			height: "1' 00\"",
+			types: ["grass", "flying"],
+		},
+		{
+			name: "Pidgeotto",
+			height: "3' 07\"",
+			types: ["fire", "flying"],
+		},
+		{
+			name: "Pidgeot",
+			height: "4' 11\"",
+			types: ["poison", "flying"],
+		},
+	];
+	function checkKeys(item) {
+		if (Object.keys(item) == ["name", "height", "types"]) {
+			return true;
+		} else {
+			return false;
+		}
 	}
-}
+
+	function add(item) {
+		if (typeof item == "object") {
+			pokemonList.push(item);
+		} else {
+			console.log("Please input an Object data type!!!");
+		}
+	}
+
+	function getAll() {
+		return pokemonList;
+	}
+
+	return {
+		add: add,
+		getAll: getAll,
+	};
+})();
+
+pokemonRepository.getAll().forEach((pokemon) => {
+	let feet = Number(pokemon.height.match(/(\d+)'/)[1]);
+	let inches = Number(pokemon.height.match(/(\d\d)"$/)[1]);
+	let total = ((feet * 12 + inches) / 39.37).toFixed(2);
+	let size;
+
+	if (total < 1) {
+		size = "It's a small pokemon.";
+	} else if (total >= 1 && total < 1.25) {
+		size = "This is a medium size pokemon.";
+	} else {
+		size = "Wow, that's big!";
+	}
+
+	document.write(
+		`<h1>${pokemon.name} has a height of ${total} meters - ${size}</h1>`
+	);
+});
+
+// for (let i = 0; i < pokemonList.length; i++) {
+// 	let convertToMeters = function (height) {
+// 		let feet = Number(pokemonList[i].height.match(/(\d+)'/)[1]);
+// 		let inches = Number(pokemonList[i].height.match(/(\d\d)"$/)[1]);
+// 		return ((feet * 12 + inches) / 39.37).toFixed(2);
+// 	};
+// 	if (convertToMeters(pokemonList[i].height) < 1.25) {
+// 		document.write(
+// 			`<h1>${pokemonList[i].name} has a height of ${convertToMeters(
+// 				pokemonList[i].height
+// 			)} meters</h1>`
+// 		);
+// 	} else {
+// 		document.write(
+// 			`<h1>${pokemonList[i].name} has a height of ${convertToMeters(
+// 				pokemonList[i].height
+// 			)} meters - Wow, that's big!</h1>`
+// 		);
+// 	}
+// }
