cc515efd2586a3d45ba872b825155bf80a3379bd
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2020-10-18 01:04:01 -0400

    added API functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,61 +1,22 @@
 let pokemonRepository = (function () {
-	let pokemonList = [
-		{
-			name: "Pidgey",
-			height: "1' 00\"",
-			types: ["grass", "flying"],
-		},
-		{
-			name: "Pidgeotto",
-			height: "3' 07\"",
-			types: ["fire", "flying"],
-		},
-		{
-			name: "Pidgeot",
-			height: "4' 11\"",
-			types: ["poison", "flying"],
-		},
-	];
-	function checkKeys(item) {
-		let itemKeys = Object.keys(item);
-		let comparisonArray = ["name", "height", "types"];
-		for (let i = 0; i < itemKeys.length; i++) {
-			if (itemKeys[i] !== comparisonArray[i]) {
-				return false;
-			}
-		}
-		return true;
-	}
+	let pokemonList = [];
+	let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
 
-	function add(item) {
-		if (typeof item == "object" && checkKeys(item)) {
-			pokemonList.push(item);
+	function add(pokemon) {
+		if (
+			typeof pokemon === "object"
+			// &&
+			// "name" in pokemon &&
+			// "height" in pokemon &&
+			// "types" in pokemon
+		) {
+			pokemonList.push(pokemon);
 		} else {
 			console.log(
 				"Please input an Object data type with the required properties!!!"
 			);
 		}
 	}
-	// Sammy's solution
-	// function add(pokemon) {
-	// 	if (
-	// 	  typeof pokemon === "object" &&
-	// 	  "name" in pokemon &&
-	// 	  "height" in pokemon &&
-	// 	  "types" in pokemon
-	// 	) {
-	// 	  repository.push(pokemon);
-	// 	} else {
-	// 	  console.log("pokemon is not correct");
-	// 	}
-	//   }
-
-	// Gurpreet's suggestion to my solution
-	// function checkKeys(item) {
-	//     let itemKeys = Object.keys(item).sort();
-	//     let comparisonArray = ["name", "height", "types"].sort();
-	//     return itemKeys === comparisonArray
-	// }
 
 	function getAll() {
 		return pokemonList;
@@ -84,7 +45,44 @@ let pokemonRepository = (function () {
 	}
 
 	function showDetails(pokemon) {
-		console.log(pokemon);
+		loadDetails(pokemon).then(function () {
+		  console.log(pokemon);
+		});
+	  }
+
+	function loadList() {
+		return fetch(apiUrl)
+			.then(function (response) {
+				return response.json();
+			})
+			.then(function (json) {
+				json.results.forEach(function (item) {
+					let pokemon = {
+						name: item.name,
+						detailsUrl: item.url,
+					};
+					add(pokemon);
+				});
+			})
+			.catch(function (e) {
+				console.error(e);
+			});
+	}
+	function loadDetails(item) {
+		let url = item.detailsUrl;
+		return fetch(url)
+			.then(function (response) {
+				return response.json();
+			})
+			.then(function (details) {
+				// Now we add the details to the item
+				item.imageUrl = details.sprites.front_default;
+				item.height = details.height;
+				item.types = details.types;
+			})
+			.catch(function (e) {
+				console.error(e);
+			});
 	}
 
 	return {
@@ -93,53 +91,13 @@ let pokemonRepository = (function () {
 		findByName: findByName,
 		addListItem: addListItem,
 		showDetails: showDetails,
+		loadList: loadList,
+		loadDetails: loadDetails,
 	};
 })();
 
-pokemonRepository.getAll().forEach((pokemon) => {
-	pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function () {
+	pokemonRepository.getAll().forEach(function (pokemon) {
+		pokemonRepository.addListItem(pokemon);
+	});
 });
-
-// Code from tasks 4 and 5
-// ----------------------------------------------------
-// pokemonRepository.getAll().forEach((pokemon) => {
-// 	let feet = Number(pokemon.height.match(/(\d+)'/)[1]);
-// 	let inches = Number(pokemon.height.match(/(\d\d)"$/)[1]);
-// 	let total = ((feet * 12 + inches) / 39.37).toFixed(2);
-// 	let size;
-
-// 	if (total < 1) {
-// 		size = "It's a small pokemon.";
-// 	} else if (total >= 1 && total < 1.25) {
-// 		size = "This is a medium size pokemon.";
-// 	} else {
-// 		size = "Wow, that's big!";
-// 	}
-
-// 	document.write(
-// 		`<h1>${pokemon.name} has a height of ${total} meters - ${size}</h1>`
-// 	);
-// });
-
-// Code from first 3 tasks
-// ---------------------------------------------------
-// for (let i = 0; i < pokemonList.length; i++) {
-// 	let convertToMeters = function (height) {
-// 		let feet = Number(pokemonList[i].height.match(/(\d+)'/)[1]);
-// 		let inches = Number(pokemonList[i].height.match(/(\d\d)"$/)[1]);
-// 		return ((feet * 12 + inches) / 39.37).toFixed(2);
-// 	};
-// 	if (convertToMeters(pokemonList[i].height) < 1.25) {
-// 		document.write(
-// 			`<h1>${pokemonList[i].name} has a height of ${convertToMeters(
-// 				pokemonList[i].height
-// 			)} meters</h1>`
-// 		);
-// 	} else {
-// 		document.write(
-// 			`<h1>${pokemonList[i].name} has a height of ${convertToMeters(
-// 				pokemonList[i].height
-// 			)} meters - Wow, that's big!</h1>`
-// 		);
-// 	}
-// }
